d6179d4200e5d5b7f3e01080cf0dfba540e7a525
ArtemHryhorovGeniusee <a.hryhorov@geniusee.com>
2025-08-25 18:31:31 +0300

    Use material-icons-extended for icons

--- composeApp/src/commonMain/kotlin/co/kluvaka/cmp/session/StartSessionScreen.kt
+++ composeApp/src/commonMain/kotlin/co/kluvaka/cmp/session/StartSessionScreen.kt
@@ -14,9 +14,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -51,10 +54,10 @@ object StartSessionScreen : Screen {
             IconButton(
               onClick = { navigator?.pop() },
               content = {
-//                Icon(
-//                  imageVector = Icons.Default.ArrowBack,
-//                  contentDescription = "Back"
-//              )
+                Icon(
+                  imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
+                  contentDescription = "Navigate back",
+                )
               }
             )
           }
