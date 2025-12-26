480011f6db5aa3c83bb773370a35bb8e22335f76
ArtemHryhorovGeniusee <a.hryhorov@geniusee.com>
2025-12-26 13:08:29 +0200

    Fix DI for Equipment module

--- composeApp/src/commonMain/kotlin/co/kluvaka/cmp/di/EquipmentModule.kt
+++ composeApp/src/commonMain/kotlin/co/kluvaka/cmp/di/EquipmentModule.kt
@@ -15,6 +15,7 @@ import co.kluvaka.cmp.features.equipment.domain.usecase.GetEquipmentById
 import co.kluvaka.cmp.features.equipment.domain.usecase.UpdateEquipment
 import co.kluvaka.cmp.features.equipment.ui.add.equipment.AddEquipmentReducer
 import co.kluvaka.cmp.features.equipment.ui.add.equipment.AddEquipmentViewModel
+import co.kluvaka.cmp.features.equipment.ui.details.EquipmentDetailsReducer
 import co.kluvaka.cmp.features.equipment.ui.details.EquipmentDetailsViewModel
 import co.kluvaka.cmp.features.equipment.ui.equipments.EquipmentsReducer
 import co.kluvaka.cmp.features.equipment.ui.equipments.EquipmentsViewModel
@@ -51,6 +52,7 @@ val equipmentModule = module {
 
   // Reducer
   single { AddEquipmentReducer() }
+  single { EquipmentDetailsReducer() }
   single { EquipmentsReducer() }
 
   // ViewModel
@@ -62,7 +64,10 @@ val equipmentModule = module {
     )
   }
   viewModel {
-    EquipmentDetailsViewModel(getEquipmentById = get())
+    EquipmentDetailsViewModel(
+      getEquipmentById = get(),
+      reducer = get(),
+    )
   }
   viewModel {
     AddEquipmentViewModel(
