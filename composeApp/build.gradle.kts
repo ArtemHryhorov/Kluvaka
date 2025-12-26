1d669b9f7ac78774fed0050e562fb207195545d3
Artem Hryhorov <65648879+ArtemHryhorov@users.noreply.github.com>
2025-12-26 13:09:22 +0200

    Fix DI for Equipment module (#32)
    * Fix DI for Equipment module
    
    * Fix DI for Equipment module
    
    ---------
    
    Co-authored-by: ArtemHryhorovGeniusee <a.hryhorov@geniusee.com>

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -78,8 +78,8 @@ android {
         applicationId = "co.kluvaka.cmp"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 1
+        versionName = "0.0.1"
     }
     packaging {
         resources {
