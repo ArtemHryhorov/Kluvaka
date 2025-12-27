43ef3e8d8d80ceae2cce29120068fdbcd8a29750
ArtemHryhorovGeniusee <a.hryhorov@geniusee.com>
2025-12-27 16:55:01 +0200

    Add empty state

--- composeApp/src/commonMain/kotlin/co/kluvaka/cmp/features/sessions/ui/history/SessionsHistoryScreen.kt
+++ composeApp/src/commonMain/kotlin/co/kluvaka/cmp/features/sessions/ui/history/SessionsHistoryScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -15,7 +16,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import co.kluvaka.cmp.features.common.ui.Dialog
@@ -24,6 +29,7 @@ import co.kluvaka.cmp.features.sessions.domain.model.FishingSession
 import co.kluvaka.cmp.features.sessions.domain.model.SessionMode
 import co.kluvaka.cmp.features.sessions.ui.active.SessionScreen
 import co.kluvaka.cmp.features.sessions.ui.history.composable.FishingSessionList
+import co.kluvaka.cmp.features.sessions.ui.history.composable.SessionsEmptyState
 import co.kluvaka.cmp.features.sessions.ui.start.session.StartSessionScreen
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -56,21 +62,25 @@ object SessionsHistoryScreen : Screen {
     ) {
       Column {
         SessionsHistoryTopBar()
-        FishingSessionList(
-          sessions = state.sessions,
-          onSessionClick = { session ->
-            if (session.isActive) {
-              navigator?.push(SessionScreen(mode = SessionMode.Active, sessionId = session.id))
-            } else {
-              session.id?.let { sessionId ->
-                navigator?.push(SessionScreen(mode = SessionMode.Completed, sessionId = sessionId))
+        if (state.sessions.isNotEmpty()) {
+          FishingSessionList(
+            sessions = state.sessions,
+            onSessionClick = { session ->
+              if (session.isActive) {
+                navigator?.push(SessionScreen(mode = SessionMode.Active, sessionId = session.id))
+              } else {
+                session.id?.let { sessionId ->
+                  navigator?.push(SessionScreen(mode = SessionMode.Completed, sessionId = sessionId))
+                }
               }
+            },
+            onSessionDelete = { session ->
+              viewModel.showDeleteDialog(session)
             }
-          },
-          onSessionDelete = { session ->
-            viewModel.showDeleteDialog(session)
-          }
-        )
+          )
+        } else {
+          SessionsEmptyState(modifier = Modifier.fillMaxWidth())
+        }
       }
       FloatingActionButton(
         modifier = Modifier
@@ -106,7 +116,11 @@ private fun SessionsHistoryTopBar() {
     windowInsets = WindowInsets(0, 0, 0, 0),
     title = {
       Text(
-        text = "Мои Рыбацкие Сессии",
+        modifier = Modifier.fillMaxWidth(),
+        text = "Журнал рыбалок",
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold,
+        fontSize = 34.sp,
       )
     }
   )
---
+++ composeApp/src/commonMain/kotlin/co/kluvaka/cmp/features/sessions/ui/history/composable/SessionsEmptyState.kt
@@ -0,0 +1,40 @@
+package co.kluvaka.cmp.features.sessions.ui.history.composable
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import kluvaka.composeapp.generated.resources.Res
+import kluvaka.composeapp.generated.resources.sessions_empty_state_background
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun SessionsEmptyState(
+  modifier: Modifier = Modifier,
+) {
+  Column(
+    modifier = modifier,
+    horizontalAlignment = Alignment.CenterHorizontally,
+  ) {
+    Text(
+      modifier = Modifier.padding(horizontal = 64.dp),
+      text = "У вас пока нет рыбалок\n Самое время добавить первую \uD83C\uDFA3",
+      textAlign = TextAlign.Center,
+    )
+    Image(
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = 64.dp),
+      painter = painterResource(Res.drawable.sessions_empty_state_background),
+      contentDescription = "Sessions empty state image",
+      contentScale = ContentScale.Crop,
+    )
+  }
+}
